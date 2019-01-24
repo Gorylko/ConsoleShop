e9337f6cf31a4a2b13f407cd1b4021037fb6ae66
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-01-25 01:09:06 +0300

    Added databases + other

---
+++ database/Condition.sql
@@ -0,0 +1,21 @@
+USE [Goodsdb]
+GO
+
+/****** Object:  Table [dbo].[Condition]    Script Date: 25.01.2019 1:07:05 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Condition](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Condition] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Condition] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ database/Location.sql
@@ -0,0 +1,21 @@
+USE [Goodsdb]
+GO
+
+/****** Object:  Table [dbo].[Location]    Script Date: 25.01.2019 1:04:54 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Location](
+	[Id] [int] NOT NULL,
+	[Location] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Location] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
