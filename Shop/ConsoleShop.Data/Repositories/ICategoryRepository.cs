ebbc10d47a28127b7ec13bb3228f073c81757ca7
Shamyakov Kirya <Shamyakov.kirya@gmail.com>
2019-02-18 00:13:25 +0300

    Added IRepository

---
+++ Shop/ConsoleShop.Data/Repositories/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleShop.Data.Repositories
+{
+    interface ICategoryRepository : IRepository<string>
+    {
+        IReadOnlyCollection<string> GetAllCategories();
+        string GetCategory(int categoryId);
+    }
+}
